4ca95edff005683ad1b6a5290457501a9e059df7
JacobMGEvans <jacobmgevans@gmail.com>
2021-05-07 22:35:33 -0500

    Config creation working, template mostly finished, WIP hook creation on init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { Command, flags } from "@oclif/command";
 import { prompt } from "enquirer";
+import * as fsp from "fs/promises";
+
+//? Maybe how I want to get root of projects
+// var path = require('path');
+// global.appRoot = path.resolve(__dirname);
 
 class GitGraft extends Command {
   static description =
@@ -16,14 +21,30 @@ class GitGraft extends Command {
   async run() {
     const { args } = this.parse(GitGraft);
     if (args.init) {
-      const response = await prompt({
+      const branchTypes = await prompt({
         type: "input",
-        name: "Branches",
-        initial: "main staging testing",
-        message: "Branches the graft should ignore?",
+        name: "branchTypes",
+        initial: "feature testing hotfix bugfix",
+        message: "Branch Gitflow types to include?",
+      });
+      const branchPattern = await prompt({
+        type: "input",
+        name: "branchPattern",
+        initial: "EXAMPLE-[0-9]{1,6}-",
+        message: "RegEx Pattern to match branches?",
       });
 
-      this.log(response);
+      await fsp.writeFile(
+        "./git-graft.json",
+        JSON.stringify({ ...branchTypes, ...branchPattern }, null, 2)
+      );
+
+      await fsp
+        .readFile(`${__dirname}/git-graft-template.js`)
+        .then((file) =>
+          fsp.writeFile(`${__dirname}/.git/hooks/commit-msg`, file)
+        );
+      // fsp.chmod("./git-graft.json", 774);
     }
   }
 }
