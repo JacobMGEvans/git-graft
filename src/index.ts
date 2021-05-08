477703ee84372d0b8fa1fb7b89f1d62635558c81
JacobMGEvans <jacobmgevans@gmail.com>
2021-05-08 15:36:41 -0500

    Template structure

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import { Command, flags } from "@oclif/command";
 import { prompt } from "enquirer";
 import * as fsp from "fs/promises";
-
-//? Maybe how I want to get root of projects
-// var path = require('path');
-// global.appRoot = path.resolve(__dirname);
+import * as path from "path";
+import execa = require("execa");
 
 class GitGraft extends Command {
   static description =
     "init will prompt for configuration inputs for the git hook, then generate git-graft.json and git hook file. ";
 
   static flags = {
-    // add --version flag to show CLI version
     version: flags.version({ char: "v" }),
     help: flags.help({ char: "h" }),
   };
@@ -34,16 +31,19 @@ class GitGraft extends Command {
         message: "RegEx Pattern to match branches?",
       });
 
+      const inDir = path.resolve(__dirname, "../templates/git-graft-template");
+      const outDir = path.join(process.cwd(), "./.git/hooks/commit-msg");
+
+      // const { stdout } = await execa("git", ["rev-parse --show-toplevel"]);
+      // this.log(stdout);
+
       await fsp.writeFile(
         "./git-graft.json",
         JSON.stringify({ ...branchTypes, ...branchPattern }, null, 2)
       );
 
-      await fsp
-        .readFile(`${__dirname}/git-graft-template.js`)
-        .then((file) =>
-          fsp.writeFile(`${__dirname}/.git/hooks/commit-msg`, file)
-        );
+      await fsp.writeFile(inDir, outDir);
+
       // fsp.chmod("./git-graft.json", 774);
     }
   }
